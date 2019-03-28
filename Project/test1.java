2e5c898ca61d4672b32296896f785c39461abc8f
Sparsh Jain <sparshjain265@gmail.com>
2019-03-28 06:30:42 +0530

    Add lex and grm files corresponding to the ast

---
+++ Project/test1.java
@@ -0,0 +1,10 @@
+// simple test for lex analysis
+// total token count = 35
+class test01 {  //3
+  public static void main(String[] a) { //11
+    System.out.println("TEST01"); //5
+    System.out.println(123); //5
+    System.out.println(); //4
+    System.out.println(true); //5
+  } //1
+} //1
